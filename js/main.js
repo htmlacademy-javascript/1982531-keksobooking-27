f138b946614153d35e8113ee122dbc6ec3e947fa
DilyaraKhayatulina <khayatulina1992@bk.ru>
2022-11-18 21:52:49 +0400

    поменяла проект

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const MIN_RANDOM_NUMBER = 6;
 const MAX_RANDOM_NUMBER = 9;
 const COMMA_RANDOM_NUMBER = 3;
-function getRandomInclusive(min, max) {
+function getRandomInteger(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   if (min < 0 || max <= min || typeof max !== 'number' || typeof min !== 'number') {
@@ -9,7 +9,7 @@ function getRandomInclusive(min, max) {
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-getRandomInclusive(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+getRandomInteger(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
 
 function getRandomFloat(min, max, comma = 2) {
   const random = Math.random().toFixed(comma) * (max - min) + min;
@@ -20,3 +20,23 @@ function getRandomFloat(min, max, comma = 2) {
 }
 getRandomFloat(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER, COMMA_RANDOM_NUMBER);
 
+const LIST_DESCRIPTION = [
+  'Поездка на море ничего не исправит, поездка в другой город ничего не исправит, выпивка ничего не исправит, всё исправишь только ты сам',
+  'Мелочи в жизни имеют огромное значение. Если для вас это мелочь, для другого это может многое значить',
+  'Каждый день вспоминай о том, как ты счастлив, и каждый твой день будет лучше предыдущего',
+  'Я верю только в себя, в сегодняшний день и свои возможности',
+  'Меня трудно найти, легко потерять и невозможно забыть',
+  'Самая красивая звезда сияет ярче всех остальных'
+];
+
+const getRandomArrayElements = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const createPost = (index) => ({
+  id: index,
+  url: `photos/${index}.jpg`,
+  description: LIST_DESCRIPTION[getRandomInteger(0, LIST_DESCRIPTION.length - 1)],
+  likes: getRandomInteger(15, 200),
+  comments: getRandomInteger(0, 200)
+});
+const similarPost = () => Array.from({length: 25}, (_, idIndex) => createPost(idIndex + 1));
+console.log(similarPost());
